b89b2d1efa9e0c97029812e33dd15c156aa92658
Kevin <kevin.haxhi@outlook.com>
2020-12-29 00:24:23 +0100

    - Added colors

--- src/Kevin_Haxhi_detyra1_fleta3.ts
+++ src/Kevin_Haxhi_detyra1_fleta3.ts
@@ -1,4 +1,10 @@
-export const test = (): void => {
-  
-  console.log('Hello World!');
+import { exec } from 'child_process';
+
+// funksion qe ben pozicionimin ne direktorine home
+
+
+const fleta3 = (): void => {
+  console.log('fleta3');
 };
+
+export default fleta3;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-import { test } from './Kevin_Haxhi_detyra1_fleta3';
+import Colors, { bgBlue } from 'colors/safe';
 
-test();
+import fleta3 from './Kevin_Haxhi_detyra1_fleta3';
+
+console.log(
+  Colors.black(
+    Colors.bgBlue(
+      'START OF EXECUTION',
+    ),
+  ),
+);
+
+// ekzekuto funksionin fleta3
+fleta3();
+
+console.log(
+  Colors.black(
+    Colors.bgRed(
+      'END OF EXECUTION',
+    ),
+  ),
+);
