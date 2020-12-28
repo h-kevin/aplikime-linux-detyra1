7270fe055f09bf1ed8d57a7f25cd03db39857c78
Kevin <kevin.haxhi@outlook.com>
2020-12-29 00:26:15 +0100

    - Fixed start and end of execution strings

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import fleta3 from './Kevin_Haxhi_detyra1_fleta3';
 console.log(
   Colors.black(
     Colors.bgBlue(
-      'START OF EXECUTION',
+      'FILLIMI I EKZEKUTIMIT',
     ),
   ),
 );
@@ -16,7 +16,7 @@ fleta3();
 console.log(
   Colors.black(
     Colors.bgRed(
-      'END OF EXECUTION',
+      'FUNDI I EKZEKUTIMIT',
     ),
   ),
 );
