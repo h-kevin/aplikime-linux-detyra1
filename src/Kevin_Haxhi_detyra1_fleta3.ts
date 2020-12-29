fba7288ee4474daf6c59a3124dea28ebe4293318
Kevin Haxhi <kevin.haxhi@softup.co>
2020-12-29 02:04:50 +0100

    Added positioning function

--- src/Kevin_Haxhi_detyra1_fleta3.ts
+++ src/Kevin_Haxhi_detyra1_fleta3.ts
@@ -1,10 +1,24 @@
 import { exec } from 'child_process';
+import { exit } from 'process';
+import { promisify } from 'util';
 
-// funksion qe ben pozicionimin ne direktorine home
+// transformo exec ne promise based
+const withPromiseExec = promisify(exec);
 
+// funksion qe ben pozicionimin ne direktorine e kerkuar
+const pozicionohu = async (pozicion: String): Promise<void> => {
+  try {
+    const { stdout } = await withPromiseExec(`cd ${pozicion}`);
+    console.log(`✔︎ Pozicionimi ne ${pozicion} u krye`);
+  } catch (error) {
+    console.log('✘ Pozicionimi deshtoi!\n\n', error);
+    
+    exit(1);
+  }
+}
 
-const fleta3 = (): void => {
-  console.log('fleta3');
+const fleta3 = async (): Promise<void> => {
+  await pozicionohu(`$HOME`);
 };
 
 export default fleta3;
--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,25 @@ import Colors, { bgBlue } from 'colors/safe';
 
 import fleta3 from './Kevin_Haxhi_detyra1_fleta3';
 
-console.log(
-  Colors.black(
-    Colors.bgBlue(
-      'FILLIMI I EKZEKUTIMIT',
+const app = async (): Promise<void> => {
+  console.log(
+    Colors.black(
+      Colors.bgBlue(
+        'FILLIMI I EKZEKUTIMIT\n',
+      ),
     ),
-  ),
-);
-
-// ekzekuto funksionin fleta3
-fleta3();
-
-console.log(
-  Colors.black(
-    Colors.bgRed(
-      'FUNDI I EKZEKUTIMIT',
+  );
+  
+  // ekzekuto funksionin fleta3
+  await fleta3();
+  
+  console.log(
+    Colors.black(
+      Colors.bgRed(
+        '\nFUNDI I EKZEKUTIMIT',
+      ),
     ),
-  ),
-);
+  );
+};
+
+app();
